1a396c74421a6ed97c32b1077c79a6e1f10cb63c
deny_crane <deny_crane@outlook.com>
2016-09-12 00:30:27 +0800

    modify note

--- stana/tuetl.py
+++ stana/tuetl.py
@@ -4,14 +4,37 @@ import tushare as ts
 import mysql.connector
 import datetime
 
+
 def loaddaydata(stockcode, bgndte, enddte):
+    print('\n\n--', datetime.datetime.now(), '\nloaddaydata:', stockcode, bgndte, enddte, )
     conn = mysql.connector.connect(user='root', password='ms@ciji1995', database='tushare')
     cursor = conn.cursor()
     engine = create_engine('mysql://root:ms@ciji1995@127.0.0.1/tushare?charset=utf8')
+    cursor.execute("select * from stockdaydata where ts_code = '%s' order by ts_date desc limit 1" % (stockcode))
+    dbrs = cursor.fetchall()
+    conn.commit()
+    if dbrs != []:
+        dbdate = dbrs[0][1]
+        dblastdate = str(dbdate)
+        dbrecord = dbrs[0][2:]
+        df1 = ts.get_h_data(code=stockcode, start=dblastdate, end=dblastdate)
+        turecord = tuple(df1.ix[dblastdate])
+        #print('dbrecord: ', dbrecord)
+        #print('turecord: ', turecord)
+        #print('bgndte:   ', bgndte)
+        if dbrecord == turecord and bgndte < dblastdate:
+            bgndte = str(dbdate + datetime.timedelta(1))
+            print('change bgndte to ', bgndte)
+        if enddte <= bgndte:
+            cursor.close()
+            conn.close()
+            print('record is already!')
+            return
+    # bgnyear = int(bgndte[0:4])
+    # endyear = int(enddte[0:4])
     print(stockcode, bgndte, enddte)
-    #bgnyear = int(bgndte[0:4])
-    #endyear = int(enddte[0:4])
-    for datayear in range(int(bgndte[0:4]), int(enddte[0:4])+1):
+    mntcnt = 0
+    for datayear in range(int(bgndte[0:4]), int(enddte[0:4]) + 1):
         yearlastday = str(datayear) + '-12-31'
         yearfirstday = str(datayear) + '-01-01'
         if enddte >= yearlastday:
@@ -23,22 +46,27 @@ def loaddaydata(stockcode, bgndte, enddte):
         else:
             bgnday = bgndte
         df = ts.get_h_data(code=stockcode, start=bgnday, end=endday, retry_count=100, pause=5)
-        print(stockcode,bgnday,endday,len(df.index))
+        if df is None:
+            continue
+        recnt = len(df.index)
+        print(stockcode, bgnday, endday, recnt)
+        mntcnt += recnt
         df.to_sql('tmpdata', engine, if_exists='replace')
         cursor.execute(
-            'delete from stockdaydata where ts_code = \'%s\' and ts_date between \'%s\' and \'%s\''
+            "delete from stockdaydata where ts_code = '%s' and ts_date between '%s' and '%s'"
             % (stockcode, bgnday, endday))
-        cursor.execute("insert into stockdaydata select \'%s\' , a.* from tmpdata a " % (stockcode))
+        cursor.execute("insert into stockdaydata select '%s' , a.* from tmpdata a " % (stockcode))
         conn.commit()
         datayear += datayear
 
     conn.commit()
     cursor.close()
     conn.close()
-    return df
+    return 'record number is :', mntcnt
+
 
 def loadstockbasics():
-    conn = mysql.connector.connect(user='root',password='ms@ciji1995',database='tushare')
+    conn = mysql.connector.connect(user='root', password='ms@ciji1995', database='tushare')
     cursor = conn.cursor()
     engine = create_engine('mysql://root:ms@ciji1995@127.0.0.1/tushare?charset=utf8')
     df = ts.get_stock_basics()
@@ -49,26 +77,22 @@ def loadstockbasics():
     conn.close()
     return df
 
+
 def loadalldaydata(code='ALL'):
     sb = loadstockbasics()
     today = datetime.date.today()
     stockenddte = today.strftime('%Y-%m-%d')
-    #print(stockenddte)
-    record ={}
+    # print(stockenddte)
+    record = {}
     codelist = [code]
     if code == 'ALL':
         codelist = sb.index
     for stockcode in codelist:
         bgnpre = str(sb['timeToMarket'][stockcode])
         if len(bgnpre) == 8:
-            stockbgndte = datetime.datetime.strptime(str(bgnpre),"%Y%m%d").strftime('%Y-%m-%d')
-            rs = loaddaydata(stockcode,stockbgndte,stockenddte)
-            record[stockcode]=len(rs.index)
-    #print(record)
+            stockbgndte = datetime.datetime.strptime(str(bgnpre), "%Y%m%d").strftime('%Y-%m-%d')
+            rs = loaddaydata(stockcode, stockbgndte, stockenddte)
+            #record[stockcode] = len(rs.index)
+            # print(record)
 
-
-
-
-
-
-#loaddaydata('600036')
+# loaddaydata('600036')
